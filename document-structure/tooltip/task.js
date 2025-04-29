5880ca9002aa5b60fb1112a27cc5905ee510b55d
andanilin <andanilin7@gmail.com>
2025-04-29 23:09:00 +0200

    Корректировка задания tooltip

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,42 +1,52 @@
 const hasTooltips = document.querySelectorAll('.has-tooltip');
-const atributes = ['top','right','left', 'bottom' ];
+const attributes = ['top', 'right', 'left', 'bottom'];
 
-hasTooltips.forEach((hasTooltip, index)=>{
-    
-    hasTooltip.insertAdjacentHTML('afterEnd', `<div class="tooltip">${hasTooltip.getAttribute('title')}</div>`)
-    hasTooltip.setAttribute('data-position', atributes[index % atributes.length]);
+hasTooltips.forEach((hasTooltip, index) => {
+    if (!hasTooltip.nextElementSibling || !hasTooltip.nextElementSibling.classList.contains('tooltip')) {
+        hasTooltip.insertAdjacentHTML('afterend', `<div class="tooltip">${hasTooltip.getAttribute('title')}</div>`);
+    }
 
-    hasTooltip.addEventListener('click', (event)=>{
+    hasTooltip.setAttribute('data-position', attributes[index % attributes.length]);
+
+    const tooltip = hasTooltip.nextElementSibling;
+
+    hasTooltip.addEventListener('click', (event) => {
         event.preventDefault();
-        const tooltips = document.querySelectorAll('.tooltip');
-        tooltips.forEach((tooltip)=>{tooltip.classList.remove("tooltip_active")});
-        tooltips[index].classList.add('tooltip_active');
 
-        const position = hasTooltip.getAttribute('data-position');
-        const rect = hasTooltip.getBoundingClientRect();
-        switch(position){
-            case 'top': 
-                tooltips[index].style.top = `${rect.top -  tooltips[index].offsetHeight}px`;
-                tooltips[index].style.left = `${rect.left + rect.width / 2 - tooltips[index].offsetWidth / 2}px`;
-                break;
-            case 'bottom': 
-                tooltips[index].style.top = `${rect.bottom}px`;
-                tooltips[index].style.left = `${rect.left + rect.width / 2 - tooltips[index].offsetWidth / 2}px`;
-                break;
-            case 'left':
-                tooltips[index].style.top = `${rect.top + rect.height / 2 - tooltips[index].offsetHeight / 2}px`;
-                tooltips[index].style.left = `${rect.left - tooltips[index].offsetWidth}px`;
-                break;
-    
-            case 'right':
-                tooltips[index].style.top = `${rect.top + rect.height / 2 - tooltips[index].offsetHeight / 2}px`;
-                tooltips[index].style.left = `${rect.right}px`;
-                break;
+        const isActive = tooltip.classList.contains('tooltip_active');
+
+        document.querySelectorAll('.tooltip').forEach((t) => t.classList.remove('tooltip_active'));
+
+        if (!isActive){
+            tooltip.classList.add('tooltip_active');
+        if (tooltip.classList.contains('tooltip_active')) {
+            const position = hasTooltip.getAttribute('data-position');
+            const rect = hasTooltip.getBoundingClientRect();
+
+            switch (position) {
+                case 'top':
+                    tooltip.style.top = `${rect.top - tooltip.offsetHeight}px`;
+                    tooltip.style.left = `${rect.left + rect.width / 2 - tooltip.offsetWidth / 2}px`;
+                    break;
+                case 'bottom':
+                    tooltip.style.top = `${rect.bottom}px`;
+                    tooltip.style.left = `${rect.left + rect.width / 2 - tooltip.offsetWidth / 2}px`;
+                    break;
+                case 'left':
+                    tooltip.style.top = `${rect.top + rect.height / 2 - tooltip.offsetHeight / 2}px`;
+                    tooltip.style.left = `${rect.left - tooltip.offsetWidth}px`;
+                    break;
+                case 'right':
+                    tooltip.style.top = `${rect.top + rect.height / 2 - tooltip.offsetHeight / 2}px`;
+                    tooltip.style.left = `${rect.right}px`;
+                    break;
+            }  
         }
+    }
     });
-
 });
-document.addEventListener('scroll', (event)=>{
-    const tooltips = document.querySelectorAll('.tooltip');
-    tooltips.forEach((tooltip)=>{tooltip.classList.remove("tooltip_active")});
+
+
+document.addEventListener('scroll', () => {
+    document.querySelectorAll('.tooltip').forEach((tooltip) => tooltip.classList.remove('tooltip_active'));
 });
